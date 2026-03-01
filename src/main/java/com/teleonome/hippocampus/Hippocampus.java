fe12188d37e1370fa1cf8058aaa32143a3c095ff
Ari Fainchtein <ari@digitalstables.com>
2026-03-01 11:35:04 +1100

    adding initial db load data

--- src/main/java/com/teleonome/hippocampus/Hippocampus.java
+++ src/main/java/com/teleonome/hippocampus/Hippocampus.java
@@ -78,7 +78,7 @@ public class Hippocampus {
 		File denomeFile = new File(Utils.getLocalDirectory() + "Teleonome.denome");
 		Identity identity;
 		boolean validJSONFormat=false;
-		long now=System.currentTimeMillis();
+		long startProcessTime=System.currentTimeMillis();
 		try{
 			String denomeFileInString = FileUtils.readFileToString(denomeFile, Charset.defaultCharset());
 			logger.info("line 82 checking the Teleonome.denome first, length=" + denomeFileInString.length() );
@@ -190,7 +190,7 @@ public class Hippocampus {
 			logger.warn(Utils.getStringException(e));
 		}finally{
 		}
-		int duration = (int) ((System.currentTimeMillis()-now)/1000);
+		int duration = (int) ((System.currentTimeMillis()-startProcessTime)/1000);
 		logger.debug("Finished loading data, it took " + Utils.getElapsedSecondsToHoursMinutesSecondsString(duration));
 	}
 	public void start() throws MqttException {
